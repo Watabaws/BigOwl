c9b85ba30269ace527f21c55ef0e18e59a8bf756
Dasha <dshifrina@stuy.edu>
2017-09-14 18:28:54 -0400

    added random occupation fxn

---
+++ occupations.py
@@ -0,0 +1,36 @@
+import random
+
+'''
+Team BigOwl: Adam Abbas, Daria Shifrina
+SoftDev pd7
+HW#03: StI/O: Divine your Destiny!
+2017-09-15
+'''
+
+cool_file = open("occupations.csv", "r")
+cool_list = cool_file.readlines()
+#print coolList
+cool_dic = {}
+i = 1
+
+while (i<len(cool_list)):
+     comma = cool_list[i].rfind(",", 0, len(cool_list[i]))
+     cool_dic[cool_list[i][:comma]] = cool_list[i][comma + 1:len(cool_list[i]) - 1]
+     i += 1
+
+#print(cool_dic)
+
+#explanation: we used cumulative probability, since random does not account for weighted probabilities as all of the values in its range have an equal probability of being picked. first, we generated a random float that stood for a random percentage(0-100%). then we summed up the dictionary probabilities in the cumulative_probability. the moment the cumulative probability exceeded the random percentage, we returned that value. this method is efficient because dictionary keys are picked in random order so there's no order ruining the randonmness(a parallel would be a bag with different amounts of colorful balls and randomly taking out one!) 
+def random_occupation():
+    c = 0 #counter for while loop
+    random_float = random.uniform(0.0, 100.0) #float within range
+    #print random_float
+    cumulative_probability = 0.0
+    while (c < len(cool_dic)):
+        cumulative_probability += float(cool_dic.values()[c])
+        if (cumulative_probability>random_float):
+            return cool_dic.keys()[c]
+        c += 1
+
+
+print random_occupation()
