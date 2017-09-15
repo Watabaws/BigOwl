18c3c5473eb7774c7d50203c577297036d6334f9
Adam Abbas <aabbas@stuy.edu>
2017-09-15 13:16:22 -0400

    Classwork 9/15

--- occupations.py
+++ occupations.py
@@ -14,11 +14,12 @@ cool_file.close()
 #print coolList
 cool_dic = {} #Close up the file
 i = 1 #this is going to be the index of our list!!
+cool_list = cool_list[:len(cool_list)-1]
 
 while (i<len(cool_list)):
      thisLine = cool_list[i] #Let's grab the current line
      comma = thisLine.rfind(",") #Let's also find the right comma - the one that seperates the number and value
-     cool_dic[thisLine[:comma]] = thisLine[comma + 1:].strip('\n') #Add the stuff before the comma (occupation) to the dictionary as a key, then add the stuff after the comma as a value!
+     cool_dic[thisLine[:comma].strip('"')] = thisLine[comma + 1:].strip('\n').strip("\r") #Add the stuff before the comma (occupation) to the dictionary as a key, then add the stuff after the comma as a value!
      i += 1 #Wouldn't want an infinite loop now would we
 
 #print(cool_dic)
@@ -26,7 +27,7 @@ while (i<len(cool_list)):
 #explanation: we used cumulative probability, since random does not account for weighted probabilities as all of the values in its range have an equal probability of being picked. first, we generated a random float that stood for a random percentage(0-100%). then we summed up the dictionary probabilities in the cumulative_probability. the moment the cumulative probability exceeded the random percentage, we returned that value. this method is efficient because dictionary keys are picked in random order so there's no order ruining the randonmness(a parallel would be a bag with different amounts of colorful balls and randomly taking out one!) 
 def random_occupation():
     c = 0 #counter for while loop
-    random_float = random.uniform(0.0, 100.0) #float within range
+    random_float = random.uniform(0.0, 99.8) #float within range
     #print random_float
     cumulative_probability = 0.0
     while (c < len(cool_dic)):
@@ -36,19 +37,20 @@ def random_occupation():
         c += 1
 
 
-print random_occupation()
+#below we are testing the accuracy of using cumulative frequency. we are running 10000 test trials to see if each occupation shows up the percentage that it is supposed to. as you can tell, it is pretty close every single time.
+        
+def proveit():
+    stuff = {} #This is going to be our occupation/amount returned 
+    ctr = 0 #Start up our loop
+    for i in cool_dic: #go through our dictionary of occupations, 
+        stuff[i] = 0
+    while ctr < 10000:
+        stuff[random_occupation()] += 1
+        ctr += 1
+    print('Occupation Frequency')
+    for occupation, frequency in stuff.items():
+        print('{} {}'.format(occupation, frequency))
 
-#below we are testing the accuracy of using cumulative frequency. we are running 1000 test trials to see if each occupation shows up the percentage that it is supposed to. as you can tell, it is pretty close every single time.
-counter = 0
-frequency_words = ""
-while(counter < 1000):
-    frequency_words += random_occupation() + "$"
-    counter +=1
 
-d = defaultdict(int)
-for word in frequency_words.split("$"):
-    d[word] += 1
 
-print('Occupation Frequency')
-for occupation, frequency in d.items():
-    print('{} {}'.format(occupation, frequency))
+proveit()
